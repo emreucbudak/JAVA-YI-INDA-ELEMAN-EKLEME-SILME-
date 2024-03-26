5d584fb6d5856533afd31038ed5984ac8f8331ce
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-26 17:15:51 +0300

    Add files via upload

---
+++ Yigin.java
@@ -0,0 +1,16 @@
+package stack;
+
+public class Yigin {
+
+	public static void main(String[] args) {
+		Stack benim = new Stack(6);
+		benim.push(35);
+		benim.push(56);
+		benim.push(58);
+		benim.push(89);
+		benim.pop();
+		benim.yazdir();
+
+	}
+
+}
